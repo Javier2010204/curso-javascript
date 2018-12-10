5e366227fa8abaf69021f883d7ce6af2772066ac
Javier2010204 <2010204javiergomez@gmail.com>
2018-12-10 11:54:43 -0600

    ejercicios

--- Ejercicio1/principal.js
+++ Ejercicio1/principal.js
@@ -1,1 +1,8 @@
-console.log("hola mundo");	
+let numero = 0;
+console.log("Ingresa un numero entero");
+numero = prompt();
+if(numero%2 == 0){
+    console.log("el numero es par");
+}else{
+    console.log("el numero es impar");
+}
---
+++ EjerciciosPrimerBloque/main.js
@@ -0,0 +1,13 @@
+let numero1;
+let numero2;
+console.log("ingrese un numero");
+numero1 = prompt();
+numero2 = prompt();
+let suma = parseInt(numero1) + parseInt(numero2);
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+console.log("la suma de " + numero1 + " y " + numero2 + " es: " + suma );
+console.log("la resta de " + numero1 + " y " + numero2 + " es: " + resta );
+console.log("la multiplicacion de " + numero1 + " y " + numero2 + " es: " + multiplicacion );
+console.log("la division de " + numero1 + " y " + numero2 + " es: " + division );
