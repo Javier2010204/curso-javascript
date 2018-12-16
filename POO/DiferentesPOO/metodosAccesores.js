c84b40ec25941521f9516a5db1fa23f7976f8101
Javier2010204 <2010204javiergomez@gmail.com>
2018-12-16 13:39:38 -0600

    Metodos accesores

---
+++ POO/DiferentesPOO/metodosAccesores.js
@@ -0,0 +1,21 @@
+class Usuario{
+
+	constructor(nombre){
+		this._name = nombre;
+	}
+
+	get name(){
+		return this._name.charAt(0).toUpperCase() + this._name.slice(1);
+	}
+
+	set name(nombre){
+		this._name = nombre;
+	}
+}
+
+
+let user = new Usuario("Javier");
+
+user.name = "Santa";
+
+console.log(user.name);
